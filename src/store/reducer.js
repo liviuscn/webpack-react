102aff9197592aa4c5fa3a08703ec79f3adf88ec
liwei <liwei@foresee.com.cn>
2020-05-06 09:45:47 +0800

    reducer修改

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,3 +1,18 @@
-export default()=>{
-    return {}
+const initialState = {
+    count: 0
+}
+//支持了reducer,先尽量不使用
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case 'increment':
+            return {
+                count: state.count + 1
+            };
+        case 'decrement':
+            return {
+                count: state.count - 1
+            };
+        default:
+            return state
+    }
 }
