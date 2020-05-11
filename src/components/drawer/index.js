8cda1c00dfedac14ebe7368e19b61c3c94b40463
liwei <liwei@foresee.com.cn>
2020-05-11 17:22:45 +0800

    样式修改

--- src/components/drawer/index.js
+++ src/components/drawer/index.js
@@ -11,38 +11,21 @@ export default (props) => {
     const className = classNames({
         "pdv-drawer": true
     })
-    return <Fragment>
-        {
-            <div className={className}>
-                <Transition in={open} timeout={300}>
-                    {state => (
-                        <Fragment>
-                            <aside
-                                className={`drawer-sidebar transform-${state}`}
-                            >
-                                <ul className='drawer-sidebar-list'>
-                                    <li>
-                                        <NavLink to="/" exact className="nav-link icon-jiantou-copy-copy">首页</NavLink>
-                                        <span></span>
-                                    </li>
-                                    <li>
-                                        <NavLink to="/login" exact className="nav-link icon-jiantou-copy-copy">登录</NavLink>
-                                        <span></span>
-                                    </li>
-                                </ul>
-                            </aside>
-                            <div
-                                onClick={props.onOpenChange}
-                                className={`drawer-mask opacity-${state}`}
-                            ></div>
-                        </Fragment>
-                    )
-                    }
-                </Transition>
-
-            </div>
-        }
-    </Fragment>
-
-
+    return <div className={className}>
+        <Transition in={open} timeout={300}>
+            {state => (
+                <Fragment>
+                    <aside className={`drawer-sidebar transform-${state}`}>
+                        <NavLink to="/" exact className="nav-link icon-jiantou-copy-copy">首页</NavLink>
+                        <NavLink to="/login" exact className="nav-link icon-jiantou-copy-copy">登录</NavLink>
+                    </aside>
+                    <div
+                        onClick={props.onOpenChange}
+                        className={`drawer-mask opacity-${state}`}
+                    />
+                </Fragment>
+            )
+            }
+        </Transition>
+    </div>
 }
