8f0db0df2b0265d5a2522927c42536e70695cdac
liwei <liwei@foresee.com.cn>
2020-05-12 15:10:16 +0800

    配置修改

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,6 +1,5 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
@@ -22,23 +21,14 @@ module.exports = merge(common, {
             'process.env.NODE_ENV': JSON.stringify('development')
         }),
         new webpack.NamedModulesPlugin(),//将使用模块的路径，缓存时hash不变，测试环境使用，速度慢
-        new webpack.HotModuleReplacementPlugin(),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[id].css',
-        })
+        new webpack.HotModuleReplacementPlugin()
     ],
     module:{
         rules:[
             {
                 test: /\.(css|less)$/,
                 use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            hmr: true,
-                        },
-                    },
+                    'style-loader',
                     'css-loader',
                     'less-loader'
                 ]
