d48014e71bac1d13304318d3e680c53b44804f4e
liweiaw <liweiaw@yonyou.com>
2021-03-14 16:54:49 +0800

    增加antd 增加阿里图标库

--- src/index.js
+++ src/index.js
@@ -1,35 +1,48 @@
-import React,{ Suspense } from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { HashRouter, Switch } from 'react-router-dom';
+import { ConfigProvider } from 'antd';
+import zhCN from 'antd/lib/locale/zh_CN';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+
 import ErrorBoundary from '@/utils/errorBoundary';
 import Spinner from '@/components/spinner'
 import Router from '@/router';
 import store from '@/store/store';
-//DLL Mapped Mode 
+//测试DLL Mapped Mode 
 import beta from '@/assets/js/beta'
-//DLL Scoped Mode
+//测试DLL Scoped Mode
 import a from 'assets/src/assets/js/a'
 import b from 'assets/src/assets/js/b'
 import c from 'assets/src/assets/js/c'
 import d from 'assets/src/assets/js/d'
-import styles from  '@/assets/style/global.less'
+
+import styles from '@/assets/style/global.less'
+
+moment.locale('zh-cn');
 
 console.log(beta, 'beta');
 console.log(a, b, c, d, 'a,b,c,d');
 
-ReactDOM.render(
-    //绑定redux、热加载
-    <Provider store={store} className={styles.root}>
+const App = () => {
+    return <Provider store={store} className={styles.root}>
         <HashRouter>
             <ErrorBoundary>
                 <Suspense fallback={<Spinner />}>
-                    <Switch>
-                        <Router />
-                    </Switch>
+                    <ConfigProvider locale={zhCN}>
+                        <Switch>
+                            <Router />
+                        </Switch>
+                    </ConfigProvider>
                 </Suspense>
             </ErrorBoundary>
         </HashRouter>
-    </Provider>,
+    </Provider>
+}
+
+ReactDOM.render(
+    <App />,
     document.getElementById('root'),
 )
